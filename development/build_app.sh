18fd481eae7195d90e3eaeffe9044d2e8dcd936b
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2018-02-21 07:31:51 +0100

    refact: move to github
    Signed-off-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- development/build_app.sh
+++ development/build_app.sh
@@ -3,7 +3,7 @@
 echo "Building version: $1"
 source /reroils/reroils/bin/activate && \
 cd reroils/src && \
-git clone https://gitlab.rero.ch/rero21/reroils-data.git && \
+git clone https://github.com/rero/reroils-data.git && \
 cd reroils-data; pip install .[all]; pip install git+https://github.com/inveniosoftware/invenio-circulation.git#egg=invenio-circulation
 
 
