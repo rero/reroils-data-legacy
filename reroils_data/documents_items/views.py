a66d759d668ec6ca6195c672d91bf3285f86d276
Aly Badr <aly.badr@rero.ch>
2018-04-13 11:34:35 +0200

    feat: display request dropdown button for checked-out items
    Signed-off-by: Aly Badr <aly.badr@rero.ch>

--- reroils_data/documents_items/views.py
+++ reroils_data/documents_items/views.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of organisations.
-# Copyright (C) 2018 RERO.
+# This file is part of Invenio.
+# Copyright (C) 2017 RERO.
 #
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, RERO does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 """Blueprint used for loading templates.
 
@@ -14,6 +32,8 @@ this file.
 from __future__ import absolute_import, print_function
 
 from flask import Blueprint
+from flask_login import current_user
+
 
 blueprint = Blueprint(
     'reroils_data_documents_items',
@@ -21,3 +41,12 @@ blueprint = Blueprint(
     template_folder='templates',
     static_folder='static',
 )
+
+
+@blueprint.app_template_filter()
+def can_request(item):
+    if current_user.is_authenticated:
+        for holding in item.get('_circulation', {}).get('holdings', []):
+            if item.get('_circulation').get('status') == 'on_loan':
+                return True
+    return False
